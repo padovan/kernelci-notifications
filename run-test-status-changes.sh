73adc570b55ee7ebd9ad78eda763caa6067e9a9b
Gustavo Padovan <gustavo.padovan@collabora.com>
2025-02-19 17:30:36 -0300

    start using the summary jinja template
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.com>

--- run-test-status-changes.sh
+++ run-test-status-changes.sh
@@ -4,6 +4,6 @@ CODE_DIR='/home/gus/p/ecosystem/kernelci-notifications'
 
 cd $CODE_DIR
 source .venv/bin/activate
-./generate-notifications --send --to kernelci-results@groups.io --cc gus@collabora.com --yes tests --run
+./generate-notifications --send --to kernelci-results@groups.io --cc gus@collabora.com --yes summary
 
 echo "run tests status: finished execution at $(date)"
