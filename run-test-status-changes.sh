1acf773fa44f7fabf21f3c18c4c1668372fba70a
Gustavo Padovan <gus@192.168.1.10>
2025-02-22 11:27:17 -0300

    add --add-mailing-lists
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.com>

--- run-test-status-changes.sh
+++ run-test-status-changes.sh
@@ -4,6 +4,5 @@ CODE_DIR='/home/gus/p/ecosystem/kernelci-notifications-deploy'
 
 cd $CODE_DIR
 source .venv/bin/activate
-./generate-notifications --send --to kernelci-results@groups.io --cc gus@collabora.com --yes summary
-
+./generate-notifications --send --add-mailing-lists --cc gus@collabora.com --yes summary
 echo "run tests status: finished execution at $(date)"
